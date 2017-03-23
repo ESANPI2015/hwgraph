e586643413bbf639e1a9b299727d2ad085413c6c
Moritz Schilling <moritz.schilling@dfki.de>
2017-03-23 19:11:10 +0100

    Temporary fix for compilation
    Signed-off-by: Moritz Schilling <moritz.schilling@dfki.de>

--- README.md
+++ README.md
@@ -3,12 +3,16 @@
 This library derives from the generic concepts formulated in the hypergraph library concepts related to the hardware components and architecture of (robotic) systems.
 
 For example, the processing hardware of a robot can be described by the means of
+
 * Devices
 * Interfaces
 * Busses
+
 These entities have the following general relations defined:
+
 * Devices have Interfaces
 * Busses connect Interfaces
+
 With these concepts both processing devices, there interfaces and their interconnection can be described.
 
 ## Status
--- tools/gen_vhdl_toplvl.cpp
+++ tools/gen_vhdl_toplvl.cpp
@@ -21,45 +21,45 @@ int main(void)
     std::cout << Hyperedge::serialize(&hwgraph);
 
     // Get the superclass of all devices
-    auto superDevice = hwgraph.devices();
+    //auto superDevice = hwgraph.devices();
     // Find specific device(s)
-    auto devices = superDevice->membersOf()->members("TestDevice");
+    //auto devices = superDevice->membersOf()->members("TestDevice");
     // For each of these devices
-    for (auto deviceIt : devices)
-    {
-        auto device = deviceIt.second;
-        // Produce VHDL skeleton
-        std::cout << "-- CHWG to VHDL TOPLVL Generator --\n";
-        std::cout << "-- libraries here --\n";
-        std::cout << "-- packages here --\n";
-        std::cout << "entity " << device->label() << " is\n";
-        std::cout << "port(\n";
-        std::cout << "-- interfaces here --\n";
-        // Get all interfaces of this device
-        // That means: get all "has" relations (has(x,a), has(y,b) ...)
-        // For each: filter according to the given device (has(x,a).membersOf() && device)
-        // Find the "has" relations which contain the device we are currently interested in.
-        // This means we
-        // I. get all supersets of device (which contains all has(device, *) relations and other supersets)
-        // II. get all has relations (only contains has(*,*) relations)
-        // III. intersect: Contains only has(device,*) relations
-        // IV. get the membersOf(): Contains (has(device, *), device, *)
-        // V. Intersect with all interfaces: Contains all interfaces of device
-        auto query = (device->supersOf()->intersect(hwgraph.labelPartOf("has")))->membersOf()->intersect(hwgraph.interfaces()->membersOf());
-        for (auto memberIt : query->members())
-        {
-            auto interface = memberIt.second;
-            std::cout << "-- Interface: " << interface->label() << std::endl;
-            std::cout << "-- TODO: How do we know what pins/groups and directions we have to assign here?\n";
-        }
-        std::cout << ");\n";
-        std::cout << "end " << device->label() << ";\n";
-        std::cout << "architecture BEHAVIOURAL of " << device->label() << " is\n";
-        std::cout << "-- signals here --\n";
-        std::cout << "\nbegin\n";
-        std::cout << "-- processes here --\n";
-        std::cout << "end BEHAVIORAL;\n";
-    }
+    //for (auto deviceIt : devices)
+    //{
+    //    auto device = deviceIt.second;
+    //    // Produce VHDL skeleton
+    //    std::cout << "-- CHWG to VHDL TOPLVL Generator --\n";
+    //    std::cout << "-- libraries here --\n";
+    //    std::cout << "-- packages here --\n";
+    //    std::cout << "entity " << device->label() << " is\n";
+    //    std::cout << "port(\n";
+    //    std::cout << "-- interfaces here --\n";
+    //    // Get all interfaces of this device
+    //    // That means: get all "has" relations (has(x,a), has(y,b) ...)
+    //    // For each: filter according to the given device (has(x,a).membersOf() && device)
+    //    // Find the "has" relations which contain the device we are currently interested in.
+    //    // This means we
+    //    // I. get all supersets of device (which contains all has(device, *) relations and other supersets)
+    //    // II. get all has relations (only contains has(*,*) relations)
+    //    // III. intersect: Contains only has(device,*) relations
+    //    // IV. get the membersOf(): Contains (has(device, *), device, *)
+    //    // V. Intersect with all interfaces: Contains all interfaces of device
+    //    auto query = (device->supersOf()->intersect(hwgraph.labelPartOf("has")))->membersOf()->intersect(hwgraph.interfaces()->membersOf());
+    //    for (auto memberIt : query->members())
+    //    {
+    //        auto interface = memberIt.second;
+    //        std::cout << "-- Interface: " << interface->label() << std::endl;
+    //        std::cout << "-- TODO: How do we know what pins/groups and directions we have to assign here?\n";
+    //    }
+    //    std::cout << ");\n";
+    //    std::cout << "end " << device->label() << ";\n";
+    //    std::cout << "architecture BEHAVIOURAL of " << device->label() << " is\n";
+    //    std::cout << "-- signals here --\n";
+    //    std::cout << "\nbegin\n";
+    //    std::cout << "-- processes here --\n";
+    //    std::cout << "end BEHAVIORAL;\n";
+    //}
 
     Hyperedge::cleanup();
 }
